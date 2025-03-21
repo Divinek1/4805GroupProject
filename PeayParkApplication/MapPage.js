2531165960e22fa78beecdb09ed3325ade3bb9eb
Divinek1 <98403684+Divinek1@users.noreply.github.com>
2025-03-21 12:18:40 -0500

    Added a map. Apple Maps gets pulled up because of the default MapView component

--- PeayParkApplication/MapPage.js
+++ PeayParkApplication/MapPage.js
@@ -1,8 +1,31 @@
-// This is the map page
 
-const MapPage = () => {
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
+const MapPage = () => { // TADA! DEFAULT MAP
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825, // Example latitude
+                    longitude: -122.4324, // Example longitude
+                    latitudeDelta: 0.0922, // Zoom level
+                    longitudeDelta: 0.0421, // Zoom level
+                }}
+            />
+        </View>
+    );
 };
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject, // This makes the map take up the whole screen
+    },
+});
 
 export default MapPage;
